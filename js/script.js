cce125ec4af4806fae847fcf77ee64ab5830ff7e
Ronal Sánchez <ronal.sanx21@gmail.com>
2025-10-21 18:20:14 -0500

    Guardado de notas en localstore

--- js/script.js
+++ js/script.js
@@ -1,35 +1,64 @@
+document.addEventListener('DOMContentLoaded', () => {
+    mostrarNotas()
+})
 
 const textarea = document.getElementById('nota')
 const titulo_nota = document.getElementById('titulo')
 const btn_guardar = document.getElementById('btn-guardar')
 const form = document.getElementById('form')
+const contenedor_notas = document.querySelector('.notas')
 
-textarea.addEventListener('input', function() {
-    this.style.height = "auto";
-    this.style.height = this.scrollHeight + 'px';
+textarea.addEventListener('input', function () {
+    this.style.height = "auto"
+    this.style.height = this.scrollHeight + 'px'
 })
 
 form.addEventListener('submit', (e) => e.preventDefault())
 
 btn_guardar.addEventListener('click', () => {
+    const titulo = titulo_nota.value.trim()
+    const nota = textarea.value.trim()
 
-    const titulo = titulo_nota.value
-    const nota   = textarea.value
-    
-    const div_card = document.createElement('div')
-    const contenedor_notas = document.querySelector('.notas')
-    const h2_titulo = document.createElement('h2')
-    const p_nota = document.createElement('p')
+    if (titulo === "" || nota === "") {
+        alert("Debes completar ambos campos")
+        return
+    }
 
+    let notas = JSON.parse(localStorage.getItem('notas')) || []
 
-    h2_titulo.textContent = titulo
-    p_nota.textContent = nota
+    const notaObject = {
+        id: Date.now(),
+        titulo,
+        nota
+    }
 
+    notas.push(notaObject)
+    localStorage.setItem('notas', JSON.stringify(notas))
 
-    div_card.classList.add('card')
-    div_card.appendChild(h2_titulo)
-    div_card.appendChild(p_nota)
-    contenedor_notas.appendChild(div_card)
+    titulo_nota.value = ""
+    textarea.value = ""
+    textarea.style.height = "auto"
 
-
+    mostrarNotas()
 })
+
+const mostrarNotas = () => {
+    const contenedor_notas = document.querySelector('.notas')
+    contenedor_notas.innerHTML = "" // Limpia las notas previas
+
+    let notas = JSON.parse(localStorage.getItem('notas')) || []
+
+    notas.forEach((nota) => {
+        const div_card = document.createElement('div')
+        const h2_titulo = document.createElement('h2')
+        const p_nota = document.createElement('p')
+
+        h2_titulo.textContent = nota.titulo
+        p_nota.textContent = nota.nota
+
+        div_card.classList.add('card')
+        div_card.appendChild(h2_titulo)
+        div_card.appendChild(p_nota)
+        contenedor_notas.appendChild(div_card)
+    })
+}
