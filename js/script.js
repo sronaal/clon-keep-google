6b4cb62ff9f0f676aa01717c3d549f37cbc37f00
Ronal Sánchez <ronal.sanx21@gmail.com>
2025-10-21 17:04:56 -0500

    Formulario Nota

--- js/script.js
+++ js/script.js
@@ -1,7 +1,35 @@
-const textarea = document.getElementById('nota')
 
+const textarea = document.getElementById('nota')
+const titulo_nota = document.getElementById('titulo')
+const btn_guardar = document.getElementById('btn-guardar')
+const form = document.getElementById('form')
 
 textarea.addEventListener('input', function() {
     this.style.height = "auto";
     this.style.height = this.scrollHeight + 'px';
+})
+
+form.addEventListener('submit', (e) => e.preventDefault())
+
+btn_guardar.addEventListener('click', () => {
+
+    const titulo = titulo_nota.value
+    const nota   = textarea.value
+    
+    const div_card = document.createElement('div')
+    const contenedor_notas = document.querySelector('.notas')
+    const h2_titulo = document.createElement('h2')
+    const p_nota = document.createElement('p')
+
+
+    h2_titulo.textContent = titulo
+    p_nota.textContent = nota
+
+
+    div_card.classList.add('card')
+    div_card.appendChild(h2_titulo)
+    div_card.appendChild(p_nota)
+    contenedor_notas.appendChild(div_card)
+
+
 })
